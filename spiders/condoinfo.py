a9162c625f85bcdba2ba8c64174af4fa7f7dd73c
law-brandon <108772362+law-brandon@users.noreply.github.com>
2022-07-14 22:26:03 -0400

    Added maintenance fees as a scraping parameter

--- spiders/condoinfo.py
+++ spiders/condoinfo.py
@@ -11,7 +11,7 @@ class CondoinfoSpider(scrapy.Spider):
         condo_addresses = response.xpath('//*[contains(@class, "styles___Address-sc-54qk44-13 gTwVlm")]/text()').getall()
         condo_specs = response.xpath('//*[contains(@class, "styles___InfoHolder-sc-54qk44-7 jtFhfz")]/text()').getall()
         condo_size = response.xpath('//*[contains(@class, "styles___Size-sc-54qk44-8 KRKbD")]/text()').getall()
-        #maint_fee = response.xpath('//*[contains(@class, "styles___MaintHolder-sc-54qk44-10 laUWhE")]/text()').getall()
+        maint_fees = response.xpath('//*[contains(@class, "styles___MaintHolder-sc-54qk44-10 laUWhE")]/div/text()').getall()
         condo_ID = response.xpath('//*[contains(@class, "styles___Mls-sc-54qk44-12 hRmahP")]/text()').getall()
 
         yield {
@@ -19,6 +19,6 @@ class CondoinfoSpider(scrapy.Spider):
             'condo_addresses' : condo_addresses,
             'condo_specs' : condo_specs,
             'condo_size' : condo_size,
-            #'maint_fee' : maint_fee,
+            'maint_fees' : maint_fees,
             'condo_ID' : condo_ID
         }
